89f3fd548477a9aa829385ccf77d14328a417da2
dnlgby <dnlgby@gmail.com>
2019-09-19 19:53:31 +0300

    Deploy

--- deploy/update.sh
+++ deploy/update.sh
@@ -2,11 +2,11 @@
 
 set -e
 
-PROJECT_MANAGE_PATH='/usr/local/apps/dabert-rest-api/dabert'
+PROJECT_MANAGE_PATH='/usr/local/apps/whatsapp_groups_api/app'
 
 git pull
 $PROJECT_MANAGE_PATH/env/bin/python manage.py migrate
 $PROJECT_MANAGE_PATH/env/bin/python manage.py collectstatic --noinput
-supervisorctl restart dabert_api
+supervisorctl restart app
 
 echo "DONE! :)"
